41ec0faee4119a0d6a2721591a7cfd00f72873ed
Ritesh <rdtailor@gmail.com>
2018-05-23 16:59:16 +0530

    Shops, Shop Details, Lockscreen(50%)

--- src/app/shops/shops.component.ts
+++ src/app/shops/shops.component.ts
@@ -10,6 +10,7 @@ import { SuperAdminService } from '../services/super-admin.service';
 export class ShopsComponent implements OnInit {
 
   shopsData: any;
+  shopsDataN: any;
 
   constructor(private superadminservice: SuperAdminService) {
 
@@ -18,7 +19,60 @@ export class ShopsComponent implements OnInit {
   ngOnInit() {
     this.superadminservice.getShopsData().subscribe(
       res => {
+        this.shopsData = '';
         this.shopsData = res['records'];
+      }
+    );
+
+    this.superadminservice.getNewShops().subscribe(
+      res => {
+        console.log('newData');
+        this.shopsDataN = '';
+        this.shopsDataN = res['records'];
+      }
+    );
+  }
+
+  SetActive(ShopID: number) {
+    this.superadminservice.setShopStatus(ShopID, 1).subscribe(
+      res => {
+        this.ngOnInit();
+      }
+    );
+
+  }
+
+  SetInactive(ShopID: number) {
+    this.superadminservice.setShopStatus(ShopID, 0).subscribe(
+      res => {
+        this.ngOnInit();
+      }
+    );
+  }
+
+  SetApproved(ShopID: number) {
+    this.superadminservice.setShopApproveStatus(ShopID, 1).subscribe(
+      res => {
+        this.ngOnInit();
+      }
+    );
+
+  }
+
+  SetNApproved(ShopID: number) {
+    this.superadminservice.setShopApproveStatus(ShopID, 0).subscribe(
+      res => {
+        this.ngOnInit();
+      }
+    );
+  }
+
+  SetShopDelete(ShopID: number) {
+    this.superadminservice.setShopDelete(ShopID).subscribe(
+      res => {
+        if (res['key'] === 'true') {
+          this.ngOnInit();
+        }
       }
     );
   }
